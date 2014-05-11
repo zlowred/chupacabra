fb6cf063a5c9da43f788c75c39fe809f8bb3a17c
Max Matveev <mail@maxmatveev.com>
2014-05-11 17:19:25 +0200

    Initial commit

---
+++ src/com/maxmatveev/chupacabra/Chupacabra.java
@@ -0,0 +1,10 @@
+package com.maxmatveev.chupacabra;
+
+/**
+ * Created by zlowred on 11.05.14.
+ */
+public class Chupacabra {
+    public static void main(String[] args) {
+        System.out.println("Chupacabra!!");
+    }
+}
